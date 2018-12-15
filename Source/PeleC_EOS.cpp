65a2baa261bf715a1ec1116349a1c2a36b34b07a
sireeveslbl <sireeves@lbl.gov>
2018-12-14 16:39:53 -0800

    Getting the right functions

--- Source/PeleC_EOS.cpp
+++ Source/PeleC_EOS.cpp
@@ -4,20 +4,17 @@
 #include "PeleC_Parameters.H"
 #include <cmath>
 
-#ifdef AMREX_USE_CUDA
-extern __managed__
-#else 
-extern 
-#endif
-double imw[NUM_SPECIES]; 
 
+extern "C" {
+
+AMREX_GPU_HOST_DEVICE void get_imw(double* neww); 
 AMREX_GPU_HOST_DEVICE void CKPY(double*  rho,double*  T,double*  y, int * iwrk,double*  rwrk, double *  P);
 AMREX_GPU_HOST_DEVICE void CKCVMS(double*  T, int * iwrk,double*  rwrk,double*  cvms);
 AMREX_GPU_HOST_DEVICE void CKCPMS(double*  T, int * iwrk,double*  rwrk,double*  cvms);
 AMREX_GPU_HOST_DEVICE void CKUMS(double*  T, int * iwrk,double*  rwrk,double*  ums);
 AMREX_GPU_HOST_DEVICE void CKHMS(double*  T, int * iwrk,double*  rwrk,double*  ums);
 AMREX_GPU_HOST_DEVICE void GET_T_GIVEN_EY(double*  e,double*  y, int * iwrk,double*  rwrk,double*  t, int *ierr);
-
+}
 EOS::EOS()
 {}
 
@@ -49,6 +46,8 @@ void EOS::eos_bottom()
 AMREX_GPU_DEVICE
 void EOS::eos_wb()
 {
+    amrex::Real imw[NUM_SPECIES]; 
+    get_imw(imw); 
     wbar = 1.0; 
     amrex::Real summ =0.0; 
 #pragma unroll 
