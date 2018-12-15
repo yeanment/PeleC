ac8ef10a21897900e03835583e336547ec07b3ff
sireeveslbl <sireeves@lbl.gov>
2018-12-14 16:19:30 -0800

    Some more cpp

--- Source/PeleC_EOS.cpp
+++ Source/PeleC_EOS.cpp
@@ -1,8 +1,23 @@
 #include "PeleC_EOS.H"
 #include "mechanism.h"
+#include "PeleC_index_macros.H"
+#include "PeleC_Parameters.H"
+#include <cmath>
 
-#if 0
-using namespace amrex;
+#ifdef AMREX_USE_CUDA
+extern __managed__
+#else 
+extern 
+#endif
+double imw[NUM_SPECIES]; 
+
+AMREX_GPU_HOST_DEVICE void CKPY(double*  rho,double*  T,double*  y, int * iwrk,double*  rwrk, double *  P);
+AMREX_GPU_HOST_DEVICE void CKCVMS(double*  T, int * iwrk,double*  rwrk,double*  cvms);
+AMREX_GPU_HOST_DEVICE void CKCPMS(double*  T, int * iwrk,double*  rwrk,double*  cvms);
+AMREX_GPU_HOST_DEVICE void CKUMS(double*  T, int * iwrk,double*  rwrk,double*  ums);
+AMREX_GPU_HOST_DEVICE void CKHMS(double*  T, int * iwrk,double*  rwrk,double*  ums);
+AMREX_GPU_HOST_DEVICE void GET_T_GIVEN_EY(double*  e,double*  y, int * iwrk,double*  rwrk,double*  t, int *ierr);
+
 EOS::EOS()
 {}
 
@@ -10,40 +25,91 @@ EOS::~EOS()
 {}
 
 AMREX_GPU_DEVICE 
-void eos_bottom()
+void EOS::eos_bottom()
 {
-    CKCVMS(T,iwrk,rwrk, cvi); 
-    CKCPMS(T,iwrk,rwrk, cpi); 
-    CKHMS( T,iwrk,rwrk,  hi);
+    CKCVMS(&T, &iwrk, &rwrk, cvi); 
+    CKCPMS(&T, &iwrk, &rwrk, cpi); 
+    CKHMS( &T, &iwrk, &rwrk,  hi);
     cv = 0.e0, cp = 0.e0, h = 0.e0; 
-    for(int i = 0; i < nspecies; ++i){
+    for(int i = 0; i < NUM_SPECIES; ++i){
          cv+=massfrac[i]*cvi[i];
          cp+=massfrac[i]*cpi[i]; 
          h +=massfrac[i]* hi[i]; 
     }
-    Real Cvx = wbar*cv; 
-//Do we know what Ru is here? 
+    amrex::Real Cvx = wbar*cv; 
     gam1 = (Cvx + Ru)/Cvx; 
     cs = std::sqrt(gam1*p/rho); 
     dpdr_e = p/rho;
     dpde = (gam1 - 1.0)*rho; 
     s = 1.e0; 
-    dPdr = 0.e0; 
+    dpdr = 0.e0; 
+}
+
+
+AMREX_GPU_DEVICE
+void EOS::eos_wb()
+{
+    wbar = 1.0; 
+    amrex::Real summ =0.0; 
+#pragma unroll 
+    for(int i = 0; i < NUM_SPECIES; ++i) summ+= massfrac[i]*imw[i]; 
+    wbar /= summ; 
 }
 
 
 AMREX_GPU_DEVICE
 void EOS::eos_re()
 {
+    int lierr=0; 
     eos_wb(); 
-//TODO make this work? 
-    get_T_given_eY(e, massfrac, iwrk, rwrk, T, lierr); 
-//Do we know what smallT is here? 
+    
+    GET_T_GIVEN_EY(&e, massfrac, &iwrk, &rwrk, &T, &lierr); 
     T = amrex::max(T, smallT); 
-    CKUMS(T, iwrk, rwrk, ei); 
-    CKPY(rho, T, massfrac, iwrk, rwrk, p); 
+    CKUMS(&T, &iwrk, &rwrk, ei); 
+    CKPY(&rho, &T, massfrac, &iwrk, &rwrk, &p); 
     eos_bottom(); 
 }
 
 
+/* THESE ASSUME THE ONLY PASSIVE VARS ARE THE SPECIES AND NON-USED VELOCITIES. TODO add additional passive vars*/
+AMREX_GPU_DEVICE
+int EOS::upass_map(const int i)
+{
+#if (AMREX_SPACEDIM==1)
+    if(i == 0) 
+        return 2;
+    else if(i == 1)
+        return 3; 
+    else 
+        return (i-2) + UFS;
+#elif (AMREX_SPACEDIM==2)
+    if(i == 0)
+        return 3; 
+    else 
+        return (i-1) + UFS;
+#else 
+    return i + UFS;
 #endif
+}
+
+AMREX_GPU_DEVICE 
+int EOS::qpass_map(const int i)
+{
+#if(AMREX_SPACEDIM==1)
+    if(i==0)
+        return 2; 
+    else if(i ==1); 
+        return 3; 
+    else 
+        return (i-2) + QFS; 
+#elif(AMREX_SPACEDIM==2)
+    if(i==0) 
+        return 3; 
+    else
+        return (i-1) + QFS;
+#else 
+    return i + QFS;  
+#endif
+}
+
+
--- Source/hydro/PeleC_advection.cpp
+++ Source/hydro/PeleC_advection.cpp
@@ -1,8 +1,8 @@
-//#include "PeleC_Parameters.H" 
-//#include "PeleC_index_macros.H" 
+#include "PeleC_Parameters.H" 
+#include "PeleC_index_macros.H" 
 #include "PeleC_EOS.H"
 #include "PeleC_K.H" 
-using namespace amrex; 
+
 struct Real3{
     amrex::Real x;
     amrex::Real y; 
@@ -13,19 +13,21 @@ AMREX_GPU_DEVICE
 void PeleC_ctoprim(Box const& bx, FArrayBox const& ufab, FArrayBox& qfab, FArrayBox& qauxfab)
 {
 
-#if 0
     const auto len = length(bx); 
     const auto lo  = lbound(bx); 
     const auto u   = ufab.view(lo); 
     const auto q   = qfab.view(lo); 
     const auto qa  = qauxfab.view(lo); 
 
-    const Real smallr = 1.e-19; 
-    const Real smallp = 1.e-10; 
-    Eos eos_state(nspecies); 
-    Real rho;
-    Real rhoinv;  
-    Real3 v; 
+    const amrex::Real smallr = 1.e-19; 
+    const amrex::Real smallp = 1.e-10; 
+    amrex::Real R = k_B*n_A; 
+    EOS eos_state; 
+    amrex::Real rho;
+    amrex::Real rhoinv; 
+    amrex::Real kineng; 
+    Real3 v;
+    int n, nq;  
 
     for         (int k = 0; k < len.z; ++k){
         for     (int j = 0; j < len.y; ++j){
@@ -33,7 +35,7 @@ void PeleC_ctoprim(Box const& bx, FArrayBox const& ufab, FArrayBox& qfab, FArray
                 rho = u(i,j,k,URHO); 
                 rhoinv = 1.e0/rho; 
                 v.x  = u(i,j,k,UMX)*rhoinv, v.y =  u(i,j,k,UMY)*rhoinv, v.z = u(i,j,k,UMZ)*rhoinv; 
-                Real kineng = 0.5e0*rho*(v.x*v.x + v.y*v.y + v.z*v.z); 
+                kineng = 0.5e0*rho*(v.x*v.x + v.y*v.y + v.z*v.z); 
                 q(i,j,k,QRHO) = rho; 
                 q(i,j,k,QU) = v.x; 
                 q(i,j,k,QV) = v.y;
@@ -41,8 +43,8 @@ void PeleC_ctoprim(Box const& bx, FArrayBox const& ufab, FArrayBox& qfab, FArray
 
 // Maybe #pragma unroll                 
                 for(int ipassive = 0; ipassive < npassive; ++ipassive){
-                    n = upass_map(ipassive); 
-                    nq = qpass_map(ipassive); 
+                    n = eos_state.upass_map(ipassive); 
+                    nq = eos_state.qpass_map(ipassive); 
                     q(i,j,k,nq) = u(i,j,k,n)/rhoinv; 
                 }
 
@@ -51,8 +53,9 @@ void PeleC_ctoprim(Box const& bx, FArrayBox const& ufab, FArrayBox& qfab, FArray
                 eos_state.T = u(i,j,k,UTEMP); 
                 eos_state.rho = rho; 
 //TODO figure out massfrac and auxilary vars
-//#pragma unroll 
-                for(int sp = 0; sp < nspecies; ++sp)  eos_state.massfrac[sp] = u(i,j,k,sp+UFS); 
+#pragma unroll 
+                for(int sp = 0; sp < NUM_SPECIES; ++sp)  eos_state.massfrac[sp] = u(i,j,k,sp+UFS); 
+#pragma unroll
                 for(int ax = 0; ax < naux; ++ax) eos_state.aux[ax] = q(i,j,k,ax+QFX); 
 // Call eos_re
                 eos_state.eos_re(); 
@@ -63,14 +66,14 @@ void PeleC_ctoprim(Box const& bx, FArrayBox const& ufab, FArrayBox& qfab, FArray
                 q(i,j,k,QGAME) = eos_state.p/eos_state.e*rhoinv + 1.e0; 
 
 //Auxilary Fab
+//Need small and R should be in PeleC_Parameters.H
                 qa(i,j,k,QDPDR)  = eos_state.dpdr_e; 
                 qa(i,j,k,QDPDE)  = eos_state.dpde; 
                 qa(i,j,k,QC)     = eos_state.cs; 
-                qa(i,j,k,QCSML)  = amrex::max(small, small*qaux(i,j,k,QC); 
+                qa(i,j,k,QCSML)  = amrex::max(small, small*qa(i,j,k,QC)); 
                 qa(i,j,k,QRSPEC) = R/eos_state.wbar; 
             }
         }
     }
 
-#endif
 }
