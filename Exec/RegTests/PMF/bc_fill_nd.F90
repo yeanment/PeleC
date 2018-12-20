75b05d4553d4cdc8fc2c3d61fa8bc9c37d5fb203
Marc Day <MSDay@lbl.gov>
2018-12-20 13:44:47 -0800

    Set known ncomp in fort declaration

--- Exec/RegTests/PMF/bc_fill_nd.F90
+++ Exec/RegTests/PMF/bc_fill_nd.F90
@@ -17,7 +17,7 @@ contains
     include 'AMReX_bc_types.fi'
 
     integer          :: adv_lo(3),adv_hi(3)
-    integer          :: bc(dim,2,*)
+    integer          :: bc(dim,2,NVAR)
     integer          :: domlo(3), domhi(3)
     double precision :: delta(3), xlo(3), time
     double precision :: adv(adv_lo(1):adv_hi(1),adv_lo(2):adv_hi(2),adv_lo(3):adv_hi(3),NVAR)
