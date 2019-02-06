c381097005860cc80dd99e3862af7177aa674fc7
sireeveslbl <sireeves@lbl.gov>
2019-02-05 16:14:05 -0800

    Trying and failing to kill bug

--- Source/PeleC_method_2D.cpp
+++ Source/PeleC_method_2D.cpp
@@ -17,8 +17,7 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
 
     const Box& bxg1 = grow(bx, 1); 
     const Box& bxg2 = grow(bx, 2);
-    const Box& bxg3 = grow(bx, 3);  
-    AsyncFab slope(bxg3, QVAR);
+    AsyncFab slope(bxg2, QVAR);
     auto const& slfab = slope.array();
     auto const& qfab  = q.array(); 
     auto const& qauxfab = qaux.array();
@@ -31,7 +30,7 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
 
 //===================== X slopes ===================================
     int cdir = 0; 
-    const Box& xslpbx = grow(bxg2, cdir, 1);
+    const Box& xslpbx = grow(bxg1, cdir, 1);
      
     AMREX_PARALLEL_FOR_3D (xslpbx,i,j,k, { 
         PeleC_slope_x(i,j,k, slfab, qfab);
@@ -45,7 +44,7 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
     auto const& qxmfab = qxm.array(); 
     auto const& qxpfab = qxp.array(); 
 
-    AMREX_PARALLEL_FOR_3D (bxg2, i,j,k, {
+    AMREX_PARALLEL_FOR_3D (xflxbx, i,j,k, {
         PeleC_plm_x(i, j, k, qxmfab, qxpfab, slfab, qfab, qauxfab, 
                     srcQfab, dlogafab, dx[0], dt); 
     });
@@ -64,7 +63,7 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
 //==================== Y slopes ====================================
     cdir = 1; 
     const Box& yflxbx = surroundingNodes(bxg1,cdir); 
-    const Box& yslpbx = grow(bxg2, cdir, 1);
+    const Box& yslpbx = grow(bxg1, cdir, 1);
     AsyncFab qym(yslpbx, QVAR);
     AsyncFab qyp(yslpbx, QVAR);
     auto const& qymfab = qym.array(); 
@@ -75,10 +74,10 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
     });
 
 //==================== Y interp ====================================
-    AMREX_PARALLEL_FOR_3D (bxg2, i,j,k, {
+    AMREX_PARALLEL_FOR_3D (yflxbx, i,j,k, {
         PeleC_plm_y(i,j,k, qymfab, qypfab, slfab, qfab, qauxfab, 
                 srcQfab, dlogafab, dx[1], dt); 
-    });
+   });
 
 //===================== Y initial fluxes ===========================
     AsyncFab fy(yflxbx, NVAR); 
@@ -92,33 +91,36 @@ void PeleC_umeth_2D(amrex::Box const& bx, amrex::FArrayBox const &q,
     cdir = 0; 
     AsyncFab qm(bxg2, QVAR); 
     AsyncFab qp(bxg2, QVAR);
+    const Box& tybx = grow(bx, cdir, 1); 
     auto const& qmfab = qm.array();
     auto const& qpfab = qp.array();  
 
-    AMREX_PARALLEL_FOR_3D (bxg1, i,j,k, {
+    AMREX_PARALLEL_FOR_3D (tybx, i,j,k, {
         PeleC_transy(i,j,k, qmfab, qpfab, qxmfab, qxpfab, fyfab,
                      srcQfab, qauxfab, q2fab, area2, volfab, hdt, hdtdy); 
     }); 
 
 //===================== Final Riemann problem X ====================
     const Box& xfxbx = surroundingNodes(bx, cdir); 
-    auto const& flx1fab = flx1.array(); 
+    auto const& flx1fab = flx1.array();
     AMREX_PARALLEL_FOR_3D (xfxbx, i,j,k, {
-        PeleC_cmpflx(i,j,k, qmfab, qpfab, flx1fab,q1fab, qauxfab, bcMaskfab, 0);
+      PeleC_cmpflx(i,j,k, qmfab, qpfab, flx1fab,q1fab, qauxfab, bcMaskfab, 0);
     }); 
 
 //===================== Y interface corrections ====================
     cdir = 1; 
-    AMREX_PARALLEL_FOR_3D (bxg1, i, j , k, {
+    const Box& txbx = grow(bx, cdir, 1); 
+    AMREX_PARALLEL_FOR_3D (txbx, i, j , k, {
     PeleC_transx(i,j,k, qmfab, qpfab, qymfab, qypfab, fxfab,
                  srcQfab, qauxfab, q1fab, area1, volfab, hdt, hdtdx); 
     });
 
 //===================== Final Riemann problem Y ====================
+
     const Box& yfxbx = surroundingNodes(bx, cdir);
     auto const& flx2fab = flx2.array();  
     AMREX_PARALLEL_FOR_3D (yfxbx, i, j, k, {
-        PeleC_cmpflx(i,j,k, qmfab, qpfab, flx2fab, q2fab, qauxfab, bcMaskfab, 1); 
+      PeleC_cmpflx(i,j,k, qmfab, qpfab, flx2fab, q2fab, qauxfab, bcMaskfab, 1); 
     });
 
 //===================== Construct p div{U} =========================
