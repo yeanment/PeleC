f613067b06c3272995fa81591f594de192c8f236
Hariswaran Sitaraman <hsitaram@el1.ib0.cm.hpc.nrel.gov>
2021-04-20 14:07:39 -0600

    reverting to exact flux at stair step

--- Source/MOL_SS.cpp
+++ Source/MOL_SS.cpp
@@ -46,28 +46,28 @@ pc_compute_hyp_mol_flux_SS(
                     cbox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
             {
                     mol_slope_SS(i, j, k, bdim, q_idx, q, qaux, dq,flags);
-                    });
+            });
         }
 
         const amrex::Box tbox = amrex::grow(cbox, dir, -1);
         const amrex::Box ebox = amrex::surroundingNodes(tbox, dir);
         amrex::ParallelFor(
-                ebox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
+               ebox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept 
         {
-                const int ii = i - bdim[0];
-                const int jj = j - bdim[1];
-                const int kk = k - bdim[2];
+            const int ii = i - bdim[0];
+            const int jj = j - bdim[1];
+            const int kk = k - bdim[2];
 
-                if(flags(i,j,k).isRegular() && flags(ii,jj,kk).isRegular())
-                {
+            if(flags(i,j,k).isRegular() && flags(ii,jj,kk).isRegular())
+            {
 
                 amrex::Real qtempl[5 + NUM_SPECIES] = {0.0};
                 qtempl[R_UN] =
-                q(ii, jj, kk, q_idx[0]) +
-                0.5 * ((dq(ii, jj, kk, 1) - dq(ii, jj, kk, 0)) / q(ii, jj, kk, QRHO));
+                    q(ii, jj, kk, q_idx[0]) +
+                    0.5 * ((dq(ii, jj, kk, 1) - dq(ii, jj, kk, 0)) / q(ii, jj, kk, QRHO));
                 qtempl[R_P] =
-                q(ii, jj, kk, QPRES) +
-                0.5 * (dq(ii, jj, kk, 0) + dq(ii, jj, kk, 1)) * qaux(ii, jj, kk, QC);
+                    q(ii, jj, kk, QPRES) +
+                    0.5 * (dq(ii, jj, kk, 0) + dq(ii, jj, kk, 1)) * qaux(ii, jj, kk, QC);
                 qtempl[R_UT1] = q(ii, jj, kk, q_idx[1]) + 0.5 * dq(ii, jj, kk, 2);
                 qtempl[R_UT2] = q(ii, jj, kk, q_idx[2]) + 0.5 * dq(ii, jj, kk, 3);
                 qtempl[R_RHO] = 0.0;
@@ -195,191 +195,36 @@ pc_compute_hyp_mol_flux_SS(
                 {
                     flx[dir](i, j, k, ivar) += flux_tmp[ivar] * area[dir](i, j, k);
                 }
-                }
-                else
+            }
+            else
+            {
+                amrex::Real flux_tmp[NVAR] = {0.0};
+                amrex::Real pres_inside;
+
+                //at least one cell should be regular
+                if(flags(i,j,k).isRegular() || flags(ii,jj,kk).isRegular())
                 {
-                    amrex::Real qtempl[5 + NUM_SPECIES] = {0.0};
-                    amrex::Real qtempr[5 + NUM_SPECIES] = {0.0};
-                    amrex::Real spl[NUM_SPECIES]={0.0};
-                    amrex::Real spr[NUM_SPECIES]={0.0};
-                    amrex::Real eos_state_rho;
-                    amrex::Real eos_state_p;
-                    amrex::Real eos_state_e;
-                    amrex::Real eos_state_cs;
-                    amrex::Real eos_state_gamma;
-                    amrex::Real eos_state_T;
-                    amrex::Real cavg;
-                    amrex::Real csmall;
-                    amrex::Real rhoe_l,rhoe_r;
-                    amrex::Real gamc_l,gamc_r;
-                    amrex::Real ustar = 0.0;
-                    amrex::Real flux_tmp[NVAR] = {0.0};
-                    amrex::Real tmp0;
-                    amrex::Real tmp1;
-                    amrex::Real tmp2;
-                    amrex::Real tmp3;
-                    amrex::Real tmp4;
-
-                    //at least one cell should be regular
-                    if(flags(i,j,k).isRegular() || flags(ii,jj,kk).isRegular())
+                    if(flags(i,j,k).isRegular() && !flags(ii,jj,kk).isRegular())
                     {
-                        if(flags(i,j,k).isRegular() && !flags(ii,jj,kk).isRegular())
-                        {
+                        pres_inside=q(i, j, k, QPRES);
+                    }
+                    else
+                    {
+                        pres_inside=q(ii, jj, kk, QPRES);
+                    }
 
-                            qtempr[R_UN]  = q(i, j, k, q_idx[0]);
-                            qtempr[R_P]   = q(i, j, k, QPRES);
-                            //qtempr[R_UT1] = q(i, j, k, q_idx[1]);
-                            //qtempr[R_UT2] = q(i, j, k, q_idx[2]);
-                            qtempr[R_UT1] = 0.0;
-                            qtempr[R_UT2] = 0.0;
-                            qtempr[R_RHO] = 0.0;
+                    for (int n = 0; n < NVAR; n++) 
+                    {
+                        flux_tmp[n] = 0.0; //0 flux at wall
+                    }
+                    flux_tmp[UMX+dir] = pres_inside;
 
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                qtempr[R_Y + n] =
-                                    q(i, j, k, QFS + n) * q(i, j, k, QRHO);
-                                qtempr[R_RHO] += qtempr[R_Y + n];
-                            }
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                qtempr[R_Y + n] = qtempr[R_Y + n] / qtempr[R_RHO];
-                            }
-
-                            cavg   = qaux(i, j, k, QC);
-                            csmall = qaux(i, j, k, QCSML);
-
-                            eos_state_rho = qtempr[R_RHO];
-                            eos_state_p = qtempr[R_P];
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                spr[n] = qtempr[R_Y + n];
-                            }
-
-                            auto eos = pele::physics::PhysicsType::eos();
-                            eos.RYP2T(eos_state_rho, spr, eos_state_p, eos_state_T);
-                            eos.RTY2E(eos_state_rho, eos_state_T, spr, eos_state_e);
-                            eos.RTY2G(eos_state_rho, eos_state_T, spr, eos_state_gamma);
-                            eos.RTY2Cs(eos_state_rho, eos_state_T, spr, eos_state_cs);
-
-                            const amrex::Real rhoe_r = eos_state_rho * eos_state_e;
-                            const amrex::Real gamc_r = eos_state_gamma;
-
-                            //copy over to left
-                            for(int n=0;n<(5+NUM_SPECIES);n++)
-                            {
-                                qtempl[n]=qtempr[n];
-                            }
-                            for(int n=0;n<NUM_SPECIES;n++)
-                            {
-                                spl[n]=spr[n];
-                            }
-                            //reflect velocities
-                            qtempl[R_UN]  = -qtempr[R_UN];
-                            qtempl[R_UT1] = -qtempr[R_UT1];
-                            qtempl[R_UT2] = -qtempr[R_UT2];
-
-                            rhoe_l=rhoe_r;
-                            gamc_l=gamc_r;
-                        }
-                        else if(!flags(i,j,k).isRegular() && flags(ii,jj,kk).isRegular())
-                        {
-                            qtempl[R_UN]  = q(ii, jj, kk, q_idx[0]);
-                            qtempl[R_P]   = q(ii, jj, kk, QPRES);
-                            //qtempl[R_UT1] = q(ii, jj, kk, q_idx[1]);
-                            //qtempl[R_UT2] = q(ii, jj, kk, q_idx[2]);
-                            qtempl[R_UT1] = 0.0;
-                            qtempl[R_UT2] = 0.0;
-                            qtempl[R_RHO] = 0.0;
-
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                qtempl[R_Y + n] =
-                                    q(ii, jj, kk, QFS + n) * q(ii, jj, kk, QRHO);
-                                qtempl[R_RHO] += qtempl[R_Y + n];
-                            }
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                qtempl[R_Y + n] = qtempl[R_Y + n] / qtempl[R_RHO];
-                            }
-
-                            cavg   = qaux(ii, jj, kk, QC);
-                            csmall = qaux(ii, jj, kk, QCSML);
-
-                            eos_state_rho = qtempl[R_RHO];
-                            eos_state_p = qtempl[R_P];
-                            for (int n = 0; n < NUM_SPECIES; n++) 
-                            {
-                                spl[n] = qtempl[R_Y + n];
-                            }
-
-                            auto eos = pele::physics::PhysicsType::eos();
-                            eos.RYP2T(eos_state_rho, spl, eos_state_p, eos_state_T);
-                            eos.RTY2E(eos_state_rho, eos_state_T, spl, eos_state_e);
-                            eos.RTY2G(eos_state_rho, eos_state_T, spl, eos_state_gamma);
-                            eos.RTY2Cs(eos_state_rho, eos_state_T, spl, eos_state_cs);
-
-                            const amrex::Real rhoe_l = eos_state_rho * eos_state_e;
-                            const amrex::Real gamc_l = eos_state_gamma;
-
-                            //copy over to right
-                            for(int n=0;n<(5+NUM_SPECIES);n++)
-                            {
-                                qtempr[n]=qtempl[n];
-                            }
-                            for(int n=0;n<NUM_SPECIES;n++)
-                            {
-                                spr[n]=spl[n];
-                            }
-                            //reflect velocities
-                            qtempr[R_UN]  = -qtempl[R_UN];
-                            qtempr[R_UT1] = -qtempl[R_UT1];
-                            qtempr[R_UT2] = -qtempl[R_UT2];
-
-                            rhoe_r=rhoe_l;
-                            gamc_r=gamc_l;
-
-                        }
-
-                        riemann(
-                                qtempl[R_RHO], qtempl[R_UN], qtempl[R_UT1], qtempl[R_UT2],
-                                qtempl[R_P], rhoe_l, spl, gamc_l, qtempr[R_RHO], qtempr[R_UN],
-                                qtempr[R_UT1], qtempr[R_UT2], qtempr[R_P], rhoe_r, spr, gamc_r,
-                                bc_test_val, csmall, cavg, ustar, flux_tmp[URHO], flux_tmp[f_idx[0]],
-                                flux_tmp[f_idx[1]], flux_tmp[f_idx[2]], flux_tmp[UEDEN],
-                                flux_tmp[UEINT], tmp0, tmp1, tmp2, tmp3, tmp4);
-
-                        for (int n = 0; n < NUM_SPECIES; n++) 
-                        {
-                            flux_tmp[UFS + n] = (ustar > 0.0) ? flux_tmp[URHO] * qtempl[R_Y + n]
-                                : flux_tmp[URHO] * qtempr[R_Y + n];
-                            flux_tmp[UFS + n] =
-                                (ustar == 0.0)
-                                ? flux_tmp[URHO] * 0.5 * (qtempl[R_Y + n] + qtempr[R_Y + n])
-                                : flux_tmp[UFS + n];
-                        }
-
-                        flux_tmp[UTEMP] = 0.0;
-                        for (int n = UFX; n < UFX + NUM_AUX; n++) 
-                        {
-                            flux_tmp[n] = (NUM_AUX > 0) ? 0.0 : flux_tmp[n];
-                        }
-                        for (int n = UFA; n < UFA + NUM_ADV; n++) 
-                        {
-                            flux_tmp[n] = (NUM_ADV > 0) ? 0.0 : flux_tmp[n];
-                        }
-
-                        for (int n = 0; n < NVAR; n++) 
-                        {
-                            flux_tmp[n] = 0.0; //0 flux at wall
-                        }
-                        flux_tmp[UMX+dir] = qtempl[R_P];
-
-                        for (int ivar = 0; ivar < NVAR; ivar++) 
-                        {
-                            flx[dir](i, j, k, ivar) += flux_tmp[ivar] * area[dir](i, j, k);
-                        }
+                    for (int ivar = 0; ivar < NVAR; ivar++) 
+                    {
+                        flx[dir](i, j, k, ivar) += flux_tmp[ivar] * area[dir](i, j, k);
                     }
                 }
+            }
         });
     }
 
